540a3fd4e58b53da11fbbcd00ba52b225744970a
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-08-26 15:11:15 +0300

    added starter publisher code

---
+++ rabbitmq_crash_course/publisher.js
@@ -0,0 +1,17 @@
+const amqp = require("amqplib");
+
+const msg = { number: 19 };
+
+async function connect() {
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    const result = await channel.assertQueue("jobs");
+    channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+    console.log(`Job sent successfully ${msg.number}`);
+  } catch (exc) {
+    console.error(exc);
+  }
+}
+
+connect();
