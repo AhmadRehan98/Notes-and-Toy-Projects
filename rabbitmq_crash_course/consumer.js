f78881b04d841641cac1341481b279432d7194c3
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-08-26 15:47:54 +0300

    add consumer.js and notes

--- rabbitmq_crash_course/consumer.js
+++ rabbitmq_crash_course/consumer.js
@@ -1,14 +1,18 @@
 const amqp = require("amqplib");
 
-const msg = { number: process.argv[2] };
-
 async function connect() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     const result = await channel.assertQueue("jobs");
-    channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
-    console.log(`Job sent successfully ${msg.number}`);
+
+    channel.consume("jobs", (message) => {
+      const input = JSON.parse(message.content.toString());
+      console.log(`Recieved job with input ${input.number}`);
+      if (input.number == 8) channel.ack(message);
+    });
+
+    console.log("Waiting for messages...");
   } catch (exc) {
     console.error(exc);
   }
